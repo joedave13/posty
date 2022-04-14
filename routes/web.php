843404813d11d43980d799aa621627c6fcfd7905
Joshua Davian <joshuadavian@gmail.com>
2022-04-14 09:43:32 +0700

    basic layout

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
+});
+
+Route::get('post', function () {
+    return view('post.index');
 });
