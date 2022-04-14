a922a4734bd0c89cb44bca6f1b647e15fcd9b8d5
Joshua Davian <joshuadavian@gmail.com>
2022-04-14 11:21:44 +0700

    authenticated state

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,18 +25,22 @@
         </ul>
 
         <ul class="flex items-center">
+            @auth
             <li>
                 <a href="" class="p-3">Joe Dave</a>
             </li>
+            <li>
+                <a href="" class="p-3">Logout</a>
+            </li>
+            @endauth
+            @guest
             <li>
                 <a href="" class="p-3">Login</a>
             </li>
             <li>
                 <a href="{{ route('register') }}" class="p-3">Register</a>
             </li>
-            <li>
-                <a href="" class="p-3">Logout</a>
-            </li>
+            @endguest
         </ul>
     </nav>
     @yield('content')
